5f6588f864e639148c363bbc8def9c80e2207758
homeworkdome <homeworkdome@outlook.com>
2023-01-11 23:06:46 +0300

    Добавьте файлы проекта.

---
+++ Задача 2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите предложение без знаков препинания, которое является палиндромом без учета пробелов и регистра");
+string str = Console.ReadLine();
+string str2 = "";
+foreach (char c in str)
+{
+    str2= c + str2;
+}
+Console.WriteLine(str2);
